6a6aed92b50ff6b24281950ebb987e6d7a5ced55
Sabina Borbely <p_sabinamaria@yahoo.com>
2021-01-19 18:35:56 +0100

    added onClickListener to btns

--- app/src/main/java/ch/sabina/trivapp/MainActivity.java
+++ app/src/main/java/ch/sabina/trivapp/MainActivity.java
@@ -4,23 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import ch.sabina.trivapp.data.AnswerListAsyncResponse;
 import ch.sabina.trivapp.data.QuestionData;
 import ch.sabina.trivapp.model.Question;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     // Log tag
     private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView questionTextview;
+    private TextView questionCounterTextview;
+    private Button trueButton;
+    private Button falseButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private int currentQuestionIndex = 0;
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       new QuestionData().getQuestions(new AnswerListAsyncResponse() {
+        nextButton = findViewById(R.id.nextButton);
+        prevButton = findViewById(R.id.prevButton);
+        trueButton = findViewById(R.id.button_true);
+        falseButton = findViewById(R.id.button_false);
+        questionCounterTextview = findViewById(R.id.counter_text);
+        questionTextview = findViewById(R.id.question_txt_view);
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+
+        questionList = new QuestionData().getQuestions(new AnswerListAsyncResponse() {
            @Override
            public void processFinished(ArrayList<Question> questionArrayList) {
                Log.d(TAG, "request received. Question list size is "+questionArrayList.size());
@@ -30,4 +54,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
+
+    }
 }
